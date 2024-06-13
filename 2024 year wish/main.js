87652f8337ae9f6ab8071adba9cab4f8ddbab53e
aiml5thsem <aiml5thsem@gmail.com>
2024-06-13 11:57:21 +0530

    first commit

---
+++ 2024 year wish/main.js
@@ -0,0 +1,11 @@
+var body = document.getElementById("mybody")
+
+
+function reverse(){
+    body.style.backgroundColor = "black";
+    body.style.color = "white"
+}
+function reverse2(){
+    body.style.backgroundColor = "white";
+    body.style.color = "black"
+}
